cf8b7be057d613c36c35666c3d087a8f18230cb3
Tris <Tris@hivemind.me>
2022-10-10 16:17:25 +1100

    Added tailwind and setup netlify

--- web/src/pages/HivemindPage/HivemindPage.tsx
+++ web/src/pages/HivemindPage/HivemindPage.tsx
@@ -1,19 +1,22 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HivemindPage = () => {
   return (
     <>
-      <MetaTags title="Hivemind" description="Hivemind page" />
-
-      <h1>HivemindPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HivemindPage/HivemindPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>hivemind</code>, link to me with `
-        <Link to={routes.hivemind()}>Hivemind</Link>`
-      </p>
+      <MetaTags
+        title="Hivemind"
+        description="Hivemind - Collective ownership"
+      />
+      <main className="mx-auto mt-16 max-w-7xl px-4 sm:mt-24">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block">Hivemind</span>{' '}
+            <span className="block text-indigo-600 xl:inline">
+              Be part of something
+            </span>
+          </h1>
+        </div>
+      </main>
     </>
   )
 }
